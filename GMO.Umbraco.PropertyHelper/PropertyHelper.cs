e0ccbbdbf2ad0174c59081e620f9c11f8679f0d8
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-07-15 17:42:07 +0000

    v1.0.0

---
+++ GMO.Umbraco.PropertyHelper/PropertyHelper.cs
@@ -0,0 +1,104 @@
+﻿using Our.Umbraco.Vorto.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace GMO.Umbraco
+{
+    /// <summary>
+    /// Extension class for <see cref="IPublishedProperty"/>'s
+    /// </summary>
+    public static class PropertyHelper
+    {
+        /// <summary>
+        /// Gets an object value from property.
+        /// Handles Vorto values, <see cref="IPublishedContent"/> values,
+        /// <see cref="GuidUdi"/>'s and collections of nodes.
+        /// </summary>
+        /// <param name="umbracoHelper">Umbraco Helper</param>
+        /// <param name="prop">Property to retrieve values from</param>
+        /// <param name="content">Parent content of published property</param>
+        /// <param name="culture">Culture of vorto property</param>
+        /// <param name="forceUrl">Always return Url from <see cref="IPublishedContent"/></param>
+        /// <param name="forceKey">Always return <see cref="Guid"/> or Id from <see cref="IPublishedContent"/></param>
+        /// <returns></returns>
+        public static object GetValueFromProperty(
+            this UmbracoHelper umbracoHelper,
+            IPublishedProperty prop,
+            IPublishedContent content,
+            string culture = null,
+            bool forceUrl = false,
+            bool forceKey = false
+        )
+        {
+            if (content.HasVortoValue(prop.PropertyTypeAlias, culture))
+            {
+                var vortoValue = content.GetVortoValue(prop.PropertyTypeAlias, culture);
+                if (vortoValue is GuidUdi guidValue)
+                {
+                    var nodeValue = umbracoHelper.TypedContent(guidValue);
+
+                    return GetNodeValue(nodeValue, forceUrl, forceKey);
+                }
+                else
+                {
+                    return vortoValue;
+                }
+            }
+            else if (prop.Value is IPublishedContent contentValue)
+            {
+                return GetNodeValue(contentValue, forceUrl, forceKey);
+            }
+            else if (prop.Value is IEnumerable<IPublishedContent> contentValueCollection)
+            {
+                return contentValueCollection.Select(x => GetNodeValue(x, forceUrl, forceKey));
+            }
+            else if (prop.Value is IHtmlString htmlString)
+            {
+                return htmlString.ToHtmlString();
+            }
+            else
+            {
+                return prop.Value;
+            }
+        }
+
+        private static object GetNodeValue(IPublishedContent node, bool forceUrl, bool forceKey)
+        {
+            if (forceUrl)
+            {
+                return node.Url;
+            }
+            else if (forceKey)
+            {
+                return GetKeyOrId(node);
+            }
+            else if (node.ItemType == PublishedItemType.Media)
+            {
+                return node.Url;
+            }
+            else
+            {
+                return GetKeyOrId(node);
+            }
+        }
+
+        private static object GetKeyOrId(IPublishedContent node)
+        {
+            var key = node.GetKey();
+
+            if (key != Guid.Empty)
+            {
+                return key.ToString();
+            }
+            else
+            {
+                return node.Id.ToString();
+            }
+        }
+    }
+}
---
+++ PropertyHelperTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PropertyHelperTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PropertyHelperTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e42a4e1-68eb-4a40-91ba-edd723a7f1d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
