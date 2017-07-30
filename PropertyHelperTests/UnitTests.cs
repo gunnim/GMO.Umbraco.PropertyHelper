34aaf9bc716ee4c3f2428bdb4e4f6d93988917d4
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-07-30 16:19:05 +0000

    First unit test

---
+++ PropertyHelperTests/UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GMO.Umbraco;
+using Moq;
+using Umbraco.Core.Models;
+using Our.Umbraco.Vorto.Models;
+
+namespace PropertyHelperTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void IsVortoPropertyWorks()
+        {
+            var vortoVal = new VortoValue();
+            var propHelper = new PrivateType(typeof(PropertyHelper));
+
+            var prop = new Mock<IPublishedProperty>();
+            prop.Setup(x => x.Value).Returns(vortoVal);
+            prop.Setup(x => x.HasValue).Returns(true);
+
+            var content = new Mock<IPublishedContent>();
+            content.Setup(
+                x => x.GetProperty(
+                    It.Is<string>(y => y == "propAlias"), 
+                    It.IsAny<bool>()))
+                .Returns(prop.Object);
+            content.Setup(
+                x => x.GetProperty(
+                    It.Is<string>(y => y == "propAlias")))
+                .Returns(prop.Object);
+
+            var result = (bool) propHelper.InvokeStatic(
+                "IsVortoProperty", 
+                new object[] { content.Object, "propAlias" }
+            );
+
+            Assert.IsTrue(result);
+        }
+    }
+}
